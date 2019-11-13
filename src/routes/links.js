3dbba784853ac32114d3ba5a1a1e087d85719e6c
Nestor Garcia <nestorgs77@gmail.com>
2019-11-13 16:05:52 -0600

    making cards and saving in the database

--- src/routes/links.js
+++ src/routes/links.js
@@ -6,10 +6,22 @@ const db_conecction = require('../database');
      res.render('links/add');
  })
 
-router.post('/add', (req, res)=>
+router.post('/add', async(req, res)=>
 {
-
+const{title,url,description}=req.body
+const newLink = {
+    title,
+    url,
+    description
+}
+await db_conecction.query('INSERT INTO links set ?',[newLink]);
 res.send('recibido');
 }
 );
+
+router.get('/', async(req,res)=>{
+  const links =  await db_conecction.query('SELECT * FROM links');
+  console.log(links);
+  res.render('links/list', {links}); 
+})
 module.exports=router ;
