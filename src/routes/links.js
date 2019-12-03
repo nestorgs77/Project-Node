25a3bcd2951fec9223fb4334e5fc38374b065a73
Nestor Garcia <nestorgs77@gmail.com>
2019-12-03 12:51:33 -0600

    Editing Links

--- src/routes/links.js
+++ src/routes/links.js
@@ -27,6 +27,11 @@ res.redirect('/links');
 
 });
 
+router.post('/readFiles', (req,res)=>{
+res.send('recibido el archivo');
+
+});
+
 router.get('/', async(req,res)=>{
   const links =  await db_conecction.query('SELECT * FROM links');
   console.log(links);
@@ -45,4 +50,28 @@ router.post('/login', (req,res)=>{
   
   res.render('links/login'); 
 })
+
+router.get('/edit/:id', async(req,res)=>{
+const{id}=req.params;
+const links =  await db_conecction.query('SELECT * FROM links WHERE id = ? ',[id]);
+
+res.render('links/edit' , {links: links[0]});
+console.log(links[0]);
+
+});
+
+router.post('/edit/:id', async(req,res)=>{
+const {id}= req.params ;
+const {title,description,url}= req.body ;
+const newLink = {
+title,
+description,
+url
+};
+console.log(newLink);
+await db_conecction.query('UPDATE links set ? WHERE id = ?',[newLink ,id]); 
+res.redirect('/links');
+
+});
+
 module.exports=router ;
